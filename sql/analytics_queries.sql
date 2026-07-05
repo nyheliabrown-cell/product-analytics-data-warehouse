158ac25dce57baa86073d84554d5b30adb5cbb47
nyheliabrown-cell <nyheliabrown@gmail.com>
2026-07-04 22:36:48 -0400

    Update analytics_queries.sql

--- sql/analytics_queries.sql
+++ sql/analytics_queries.sql
@@ -1,1 +1,31 @@
---Coming Soon
+# Daily Active Users
+SELECT
+DATE(event_time) AS activity_date,
+COUNT(DISTINCT user_id) AS daily_active_users
+FROM fact_user_events
+GROUP BY DATE(event_time);
+
+# Most Popular Features 
+SELECT
+event_type,
+COUNT(*) AS total_events
+FROM fact_user_events
+GROUP BY event_type
+ORDER BY total_events DESC;
+
+# Average Time Spent 
+SELECT
+AVG(session_duration_seconds)/60
+AS average_minutes_spent
+FROM fact_user_events;
+
+# country Engagament 
+SELECT
+country,
+COUNT(*) AS engagement
+FROM fact_user_events e
+JOIN dim_users u
+ON e.user_id = u.user_id
+GROUP BY country
+ORDER BY engagement DESC;
+
