9470ccc13c9c2e871599b426ca0c18ab86778008
nyheliabrown-cell <nyheliabrown@gmail.com>
2026-07-04 21:37:38 -0400

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,18 @@
---Coming Soon
+-- Dimension Table
+
+CREATE TABLE dim_users (
+    user_id INT PRIMARY KEY,
+    signup_date DATE,
+    country VARCHAR(50),
+    device_type VARCHAR(50)
+);
+
+-- Fact Table
+
+CREATE TABLE fact_user_events (
+    event_id INT PRIMARY KEY,
+    user_id INT,
+    event_type VARCHAR(50),
+    event_time TIMESTAMP,
+    session_duration_seconds INT
+);
